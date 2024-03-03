925b46f1cbe581394e3a79926d7ad9b1d0fa5481
smais007 <smaisshawon@gmail.com>
2024-03-03 23:05:51 +0600

    search by category function added

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,3 +1,136 @@
+const allPosts = async () => {
+  const url = `https://openapi.programming-hero.com/api/retro-forum/posts`;
+  const response = await fetch(url);
+  const data = await response.json();
+  const posts = data.posts;
+  // console.log(posts);
+
+  const discussionPosts = document.getElementById("discussion-post");
+
+  posts.forEach((post) => {
+    // console.log(post);
+    const activeStatusStyle = post.isActive
+      ? "background-color: #10B981;"
+      : "background-color: #FF3434;";
+    const discussionPostsTemplate = `<div class="flex gap-6 bg-[#F3F3F5] rounded-3xl w-[48.25rem] h-[16.875rem] pt-10 pl-10 pb-11  mb-6">
+  <div class="relative w-[75px] h-[75px] flex justify-center items-center">
+      <img src="${post.image}" alt=""
+          class="w-[72px] h-[72px] rounded-2xl">
+      <div class="active-status" style="${activeStatusStyle}"></div>
+  </div>
+  <div>
+      <div class="flex gap-5 pb-3">
+          <p class="inter text-sm font-medium  text-[#12132DCC]">#<span>${post.category}</span></p>
+          <p class="inter text-sm font-medium  text-[#12132DCC]">Author: <span>${post.author.name}</span></p>
+      </div>
+      <div>
+          <h1 class="mulish font-bold text-xl h1-color pb-4">${post.title}
+          </h1>
+          <p class="inter p-color text-base w-[35.563rem] leading-6 pb-5">${post.description}</p>
+          <hr class="border-dotted">
+      </div>
+      <div class="flex justify-between items-center pt-5">
+          <div class="flex gap-6">
+              <div class="flex  gap-3 items-center">
+                  <img src="assets/icon/comment.svg" alt="">
+                  <p class="inter text-[#12132D99]">${post.comment_count}</p>
+              </div>
+              <div class="flex  gap-3 items-center">
+                  <img src="assets/icon/view.svg" alt="">
+                  <p class="inter text-[#12132D99]">${post.view_count}</p>
+              </div>
+              <div class="flex  gap-3 items-center">
+                  <img src="assets/icon/time.svg" alt="">
+                  <p class="inter text-[#12132D99]">${post.posted_time}</p>
+              </div>
+          </div>
+          <div>
+          <button id="read-btn"><img src="assets/icon/read.svg" alt=""></button>
+
+          </div>
+      </div>
+  </div>
+</div>`;
+
+    const discussionCard = document.createElement("div");
+    discussionCard.innerHTML = discussionPostsTemplate;
+    discussionPosts.appendChild(discussionCard);
+  });
+};
+
+// Search by category
+const searchCategory = () => {
+  const searchField = document.getElementById("search-field");
+  const searchText = searchField.value;
+  console.log(searchText);
+  searchPosts(searchText);
+};
+
+const searchPosts = async (searchText) => {
+  const url = `https://openapi.programming-hero.com/api/retro-forum/posts?category=${searchText}`;
+  const response = await fetch(url);
+  const data = await response.json();
+  const posts = data.posts;
+  console.log(posts);
+
+  const discussionPosts = document.getElementById("discussion-post");
+  discussionPosts.innerHTML = "";
+
+  posts.forEach((post) => {
+    // console.log(post);
+    const activeStatusStyle = post.isActive
+      ? "background-color: #10B981;"
+      : "background-color: #FF3434;";
+    const discussionPostsTemplate = `<div class="flex gap-6 bg-[#F3F3F5] rounded-3xl w-[48.25rem] h-[16.875rem] pt-10 pl-10 pb-11  mb-6">
+  <div class="relative w-[75px] h-[75px] flex justify-center items-center">
+      <img src="${post.image}" alt=""
+          class="w-[72px] h-[72px] rounded-2xl">
+      <div class="active-status" style="${activeStatusStyle}"></div>
+  </div>
+  <div>
+      <div class="flex gap-5 pb-3">
+          <p class="inter text-sm font-medium  text-[#12132DCC]">#<span>${post.category}</span></p>
+          <p class="inter text-sm font-medium  text-[#12132DCC]">Author: <span>${post.author.name}</span></p>
+      </div>
+      <div>
+          <h1 class="mulish font-bold text-xl h1-color pb-4">${post.title}
+          </h1>
+          <p class="inter p-color text-base w-[35.563rem] leading-6 pb-5">${post.description}</p>
+          <hr class="border-dotted">
+      </div>
+      <div class="flex justify-between items-center pt-5">
+          <div class="flex gap-6">
+              <div class="flex  gap-3 items-center">
+                  <img src="assets/icon/comment.svg" alt="">
+                  <p class="inter text-[#12132D99]">${post.comment_count}</p>
+              </div>
+              <div class="flex  gap-3 items-center">
+                  <img src="assets/icon/view.svg" alt="">
+                  <p class="inter text-[#12132D99]">${post.view_count}</p>
+              </div>
+              <div class="flex  gap-3 items-center">
+                  <img src="assets/icon/time.svg" alt="">
+                  <p class="inter text-[#12132D99]">${post.posted_time}</p>
+              </div>
+          </div>
+          <div>
+          <button id="read-btn"><img src="assets/icon/read.svg" alt=""></button>
+
+          </div>
+      </div>
+  </div>
+</div>`;
+
+    const discussionCard = document.createElement("div");
+    discussionCard.innerHTML = discussionPostsTemplate;
+    discussionPosts.appendChild(discussionCard);
+  });
+};
+
+searchPosts();
+allPosts();
+
+// Latest posts
 const latestPost = async () => {
   const url = `https://openapi.programming-hero.com/api/retro-forum/latest-posts`;
   const response = await fetch(url);
@@ -5,7 +138,7 @@ const latestPost = async () => {
   //   console.log(data[0]);
   const latestPostContainer = document.getElementById("latest-post-container");
   data.forEach((post) => {
-    console.log(post);
+    // console.log(post);
     const poastCard = `<div class="w-[23.375rem] h-[30.125rem] border rounded-3xl p-6">
     <div class=" w-[20.375rem] h-[11.875rem] bg-[#12132D0D] rounded-[20px]">
         <img class="w-[20.375rem] h-[11.875rem] rounded-[20px]" src="${
@@ -44,61 +177,3 @@ const latestPost = async () => {
 };
 
 latestPost();
-
-const allPosts = async () => {
-  const url = "https://openapi.programming-hero.com/api/retro-forum/posts";
-  const response = await fetch(url);
-  const data = await response.json();
-  const posts = data.posts;
-  console.log(posts);
-
-  const discussionPosts = document.getElementById("discussion-post");
-
-  posts.forEach((post) => {
-    console.log(post);
-    const discussionPostsTemplate = `<div class="flex gap-6 bg-[#F3F3F5] rounded-3xl w-[48.25rem] h-[16.875rem] pt-10 pl-10 pb-11  mb-6">
-  <div class="relative w-[75px] h-[75px] flex justify-center items-center">
-      <img src="${post.image}" alt=""
-          class="w-[72px] h-[72px] rounded-2xl">
-      <div class="active-status"></div>
-  </div>
-  <div>
-      <div class="flex gap-5 pb-3">
-          <p class="inter text-sm font-medium  text-[#12132DCC]">#<span>${post.category}</span></p>
-          <p class="inter text-sm font-medium  text-[#12132DCC]">Author: <span>${post.author.name}</span></p>
-      </div>
-      <div>
-          <h1 class="mulish font-bold text-xl h1-color pb-4">${post.title}
-          </h1>
-          <p class="inter p-color text-base w-[35.563rem] leading-6 pb-5">${post.description}</p>
-          <hr class="border-dotted">
-      </div>
-      <div class="flex justify-between items-center pt-5">
-          <div class="flex gap-6">
-              <div class="flex  gap-3 items-center">
-                  <img src="assets/icon/comment.svg" alt="">
-                  <p class="inter text-[#12132D99]">${post.comment_count}</p>
-              </div>
-              <div class="flex  gap-3 items-center">
-                  <img src="assets/icon/view.svg" alt="">
-                  <p class="inter text-[#12132D99]">${post.view_count}</p>
-              </div>
-              <div class="flex  gap-3 items-center">
-                  <img src="assets/icon/time.svg" alt="">
-                  <p class="inter text-[#12132D99]">${post.posted_time}</p>
-              </div>
-          </div>
-          <div>
-          <button><img src="assets/icon/read.svg" alt=""></button>
-
-          </div>
-      </div>
-  </div>
-</div>`;
-    const discussionCard = document.createElement("div");
-    discussionCard.innerHTML = discussionPostsTemplate;
-    discussionPosts.appendChild(discussionCard);
-  });
-};
-
-allPosts();
