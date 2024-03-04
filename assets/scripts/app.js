cad455168cace2307a8e1fb3282ddb2bcea56a7b
smais007 <smaisshawon@gmail.com>
2024-03-04 22:40:52 +0600

    finished the assingment

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -21,54 +21,58 @@ const allPosts = async () => {
     const activeStatusStyle = post.isActive
       ? "background-color: #10B981;"
       : "background-color: #FF3434;";
-    const discussionPostsTemplate = `<div class="flex gap-6 bg-[#F3F3F5] rounded-3xl w-[48.25rem] h-[16.875rem] pt-10 pl-10 pb-11  mb-6">
-  <div class="relative w-[75px] h-[75px] flex justify-center items-center">
-      <img src="${post.image}" alt=""
-          class="w-[72px] h-[72px] rounded-2xl">
-      <div class="active-status" style="${activeStatusStyle}"></div>
-  </div>
-  <div>
-      <div class="flex gap-5 pb-3">
-          <p class="inter text-sm font-medium  text-[#12132DCC]">#<span>${
-            post.category
-          }</span></p>
-          <p class="inter text-sm font-medium  text-[#12132DCC]">Author: <span>${
-            post.author.name
-          }</span></p>
-      </div>
-      <div>
-          <h1 class="mulish font-bold text-xl h1-color pb-4">${post.title}
-          </h1>
-          <p class="inter p-color text-base w-[35.563rem] leading-6 pb-5">${
-            post.description
-          }</p>
-          <hr class="border-dotted">
-      </div>
-      <div class="flex justify-between items-center pt-5">
-          <div class="flex gap-6">
-              <div class="flex  gap-3 items-center">
-                  <img src="assets/icon/comment.svg" alt="">
-                  <p class="inter text-[#12132D99]">${post.comment_count}</p>
-              </div>
-              <div class="flex  gap-3 items-center">
-                  <img src="assets/icon/view.svg" alt="">
-                  <p class="inter text-[#12132D99]">${post.view_count}</p>
-              </div>
-              <div class="flex  gap-3 items-center">
-                  <img src="assets/icon/time.svg" alt="">
-                  <p class="inter text-[#12132D99]">${post.posted_time}</p>
-              </div>
-          </div>
-          <div>
-          <button onclick='updateReadCount(${JSON.stringify({
-            title: stringTohtml(post.title),
-            view_count: post.view_count,
-          })})' id="read-btn">
-              <img src="assets/icon/read.svg" alt="">
-          </button>
-          </div>
-      </div>
-  </div>
+    const discussionPostsTemplate = `<div
+    class="flex gap-6 bg-[#F3F3F5] rounded-3xl lg:w-[48.25rem] lg:h-[16.875rem] pt-10  lg:pl-10 pb-11  mb-6 flex-col lg:flex-row">
+    <div class="flex justify-center lg:block">
+        <div
+            class="relative w-[120px] h-[120px] lg:w-[75px] lg:h-[75px] flex justify-center items-center ">
+            <img src="${post.image}" alt=""
+                class="w-[120px] lg:w-[72px] lg:h-[72px] rounded-full lg:rounded-2xl">
+            <div class="active-status " style="${activeStatusStyle}"></div>
+        </div>
+    </div>
+    <div class="pl-5">
+        <div class="flex gap-5 pb-3">
+            <p class="inter text-sm font-medium  text-[#12132DCC]">#<span>${
+              post.category
+            }</span></p>
+            <p class="inter text-sm font-medium  text-[#12132DCC]">Author: <span>${
+              post.author.name
+            }</span></p>
+        </div>
+        <div>
+            <h1 class="mulish font-bold text-xl h1-color pb-4">${post.title}
+            </h1>
+            <p class="inter p-color text-base lg:w-[35.563rem] leading-6 pb-5">${
+              post.description
+            }</p>
+            <hr class="border-dotted">
+        </div>
+        <div class="flex justify-between items-center pt-5">
+            <div class="flex gap-6">
+                <div class="flex  gap-3 items-center">
+                    <img src="assets/icon/comment.svg" alt="">
+                    <p class="inter text-[#12132D99]">${post.comment_count}</p>
+                </div>
+                <div class="flex  gap-3 items-center">
+                    <img src="assets/icon/view.svg" alt="">
+                    <p class="inter text-[#12132D99]">${post.view_count}</p>
+                </div>
+                <div class="flex  gap-3 items-center">
+                    <img src="assets/icon/time.svg" alt="">
+                    <p class="inter text-[#12132D99]">${post.posted_time}</p>
+                </div>
+            </div>
+            <div>
+                <button onclick='updateReadCount(${JSON.stringify({
+                  title: stringTohtml(post.title),
+                  view_count: post.view_count,
+                })})' id="read-btn">
+                    <img src="assets/icon/read.svg" alt="">
+                </button>
+            </div>
+        </div>
+    </div>
 </div>`;
 
     const discussionCard = document.createElement("div");
@@ -104,55 +108,58 @@ const searchPosts = async (searchText) => {
     const activeStatusStyle = post.isActive
       ? "background-color: #10B981;"
       : "background-color: #FF3434;";
-    const discussionPostsTemplate = `<div class="flex gap-6 bg-[#F3F3F5] rounded-3xl w-[48.25rem] h-[16.875rem] pt-10 pl-10 pb-11  mb-6">
-  <div class="relative w-[75px] h-[75px] flex justify-center items-center">
-      <img src="${post.image}" alt=""
-          class="w-[72px] h-[72px] rounded-2xl">
-      <div class="active-status" style="${activeStatusStyle}"></div>
-  </div>
-  <div>
-      <div class="flex gap-5 pb-3">
-          <p class="inter text-sm font-medium  text-[#12132DCC]">#<span>${
-            post.category
-          }</span></p>
-          <p class="inter text-sm font-medium  text-[#12132DCC]">Author: <span>${
-            post.author.name
-          }</span></p>
-      </div>
-      <div>
-          <h1 class="mulish font-bold text-xl h1-color pb-4">${post.title}
-          </h1>
-          <p class="inter p-color text-base w-[35.563rem] leading-6 pb-5">${
-            post.description
-          }</p>
-          <hr class="border-dotted">
-      </div>
-      <div class="flex justify-between items-center pt-5">
-          <div class="flex gap-6">
-              <div class="flex  gap-3 items-center">
-                  <img src="assets/icon/comment.svg" alt="">
-                  <p class="inter text-[#12132D99]">${post.comment_count}</p>
-              </div>
-              <div class="flex  gap-3 items-center">
-                  <img src="assets/icon/view.svg" alt="">
-                  <p class="inter text-[#12132D99]">${post.view_count}</p>
-              </div>
-              <div class="flex  gap-3 items-center">
-                  <img src="assets/icon/time.svg" alt="">
-                  <p class="inter text-[#12132D99]">${post.posted_time}</p>
-              </div>
-          </div>
-          <div>
-          <button onclick='updateReadCount(${JSON.stringify({
-            title: stringTohtml(post.title),
-            view_count: post.view_count,
-          })})' id="read-btn">
-          <img src="assets/icon/read.svg" alt="">
-          </button>
-
-          </div>
-      </div>
-  </div>
+    const discussionPostsTemplate = `<div
+    class="flex gap-6 bg-[#F3F3F5] rounded-3xl lg:w-[48.25rem] lg:h-[16.875rem] pt-10  lg:pl-10 pb-11  mb-6 flex-col lg:flex-row">
+    <div class="flex justify-center lg:block">
+        <div
+            class="relative w-[120px] h-[120px] lg:w-[75px] lg:h-[75px] flex justify-center items-center ">
+            <img src="${post.image}" alt=""
+                class="w-[120px] lg:w-[72px] lg:h-[72px] rounded-full lg:rounded-2xl">
+            <div class="active-status " style="${activeStatusStyle}"></div>
+        </div>
+    </div>
+    <div class="pl-5">
+        <div class="flex gap-5 pb-3">
+            <p class="inter text-sm font-medium  text-[#12132DCC]">#<span>${
+              post.category
+            }</span></p>
+            <p class="inter text-sm font-medium  text-[#12132DCC]">Author: <span>${
+              post.author.name
+            }</span></p>
+        </div>
+        <div>
+            <h1 class="mulish font-bold text-xl h1-color pb-4">${post.title}
+            </h1>
+            <p class="inter p-color text-base lg:w-[35.563rem] leading-6 pb-5">${
+              post.description
+            }</p>
+            <hr class="border-dotted">
+        </div>
+        <div class="flex justify-between items-center pt-5">
+            <div class="flex gap-6">
+                <div class="flex  gap-3 items-center">
+                    <img src="assets/icon/comment.svg" alt="">
+                    <p class="inter text-[#12132D99]">${post.comment_count}</p>
+                </div>
+                <div class="flex  gap-3 items-center">
+                    <img src="assets/icon/view.svg" alt="">
+                    <p class="inter text-[#12132D99]">${post.view_count}</p>
+                </div>
+                <div class="flex  gap-3 items-center">
+                    <img src="assets/icon/time.svg" alt="">
+                    <p class="inter text-[#12132D99]">${post.posted_time}</p>
+                </div>
+            </div>
+            <div>
+                <button onclick='updateReadCount(${JSON.stringify({
+                  title: stringTohtml(post.title),
+                  view_count: post.view_count,
+                })})' id="read-btn">
+                    <img src="assets/icon/read.svg" alt="">
+                </button>
+            </div>
+        </div>
+    </div>
 </div>`;
 
     const discussionCard = document.createElement("div");
@@ -174,11 +181,10 @@ const latestPost = async () => {
   const latestPostContainer = document.getElementById("latest-post-container");
   data.forEach((post) => {
     // console.log(post);
-    const poastCard = `<div class="w-[23.375rem] h-[30.125rem] border rounded-3xl p-6">
-    <div class=" w-[20.375rem] h-[11.875rem] bg-[#12132D0D] rounded-[20px]">
-        <img class="w-[20.375rem] h-[11.875rem] rounded-[20px]" src="${
-          post.cover_image
-        }" alt="">
+    const poastCard = `<div class="lg:w-[23.375rem] lg:h-[30.125rem] border rounded-3xl p-6">
+    <div class=" lg:w-[20.375rem] lg:h-[11.875rem] bg-[#12132D0D] rounded-[20px]">
+        <img class="lg:w-[20.375rem] lg:h-[11.875rem] rounded-[20px]"
+            src="${post.cover_image}" alt="">
     </div>
     <div class="flex items-center gap-2 pt-6">
         <img src="assets/icon/calender.svg" alt="">
@@ -193,8 +199,9 @@ const latestPost = async () => {
         <p class="mulish p-color">${post.description}</p>
     </div>
     <div class="flex items-center gap-4 pt-4">
-        <img class="w-[44px] h-[44px] rounded-[50px]"
-            src="${post.profile_image}" alt="">
+        <img class="w-[44px] h-[44px] rounded-[50px]" src="${
+          post.profile_image
+        }" alt="">
         <div>
             <h1 class="mulish h1-color font-bold pb-[5px]">${
               post.author.name
